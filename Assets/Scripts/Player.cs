b6be77e90b553aa234b05f3330d593f76548469d
Antony Sajan <antonysajant@gmail.com>
2025-02-01 23:14:59 +0530

    Some edits

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,15 +2,47 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private int isLeft = 0;
+    public float speed = 10f;
+    private void Start()
     {
-        
+        transform.position = new Vector2(1.79f, -2f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(Input.touchCount);
+        if (Input.touchCount > 0)
+        {
+            swapTower();
+        }
+    }
+
+    void swapTower()
+    {
+        if (isLeft == 1)
+        {
+            while (isLeft == 1)
+            {
+                if (transform.position.x <= 1.79f)
+                {
+                    transform.Translate(Vector2.right * speed * Time.deltaTime);
+                }
+            }
+            isLeft = 0;
+        }
+        else
+        {
+            while (isLeft == 0)
+            {
+                if (transform.position.x >= -1.79f)
+                {
+                    transform.Translate(Vector2.left * speed * Time.deltaTime);
+                    
+                }
+            }
+            isLeft = 1;
+        }
     }
 }
