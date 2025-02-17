24746e2211ad121e9e2ea14b262961cbd762e4c3
Antony Sajan <antonysajant@gmail.com>
2025-02-17 22:08:47 +0530

    Tower spawn setting

--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -15,4 +15,10 @@ public class Spawn : MonoBehaviour
     {
         
     }
+
+    [ContextMenu("Spawn Tower")]
+    public void spawnTower()
+    {
+        Instantiate(towers, new Vector2(-3.221583f, spawnPoint), Quaternion.identity);
+    }
 }
