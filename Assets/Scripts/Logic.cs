ec82d5e1f7bb94c23a62b0976384cf582ec784a0
Antony Sajan <antonysajant@gmail.com>
2025-02-17 22:31:04 +0530

    Logic updated

--- Assets/Scripts/Logic.cs
+++ Assets/Scripts/Logic.cs
@@ -3,11 +3,17 @@ using UnityEngine;
 public class Logic : MonoBehaviour
 {
     [SerializeField] private GameObject towers;
+    [SerializeField] private GameObject spikes;
     [SerializeField] private float spawnPoint = 21.86f;
 
     [ContextMenu("Spawn Tower")]
     public void spawnTower()
     {
         Instantiate(towers, new Vector2(-3.221583f, spawnPoint), Quaternion.identity);
+    }
+
+    public void spawnSpike()
+    { 
+        
     }
 }
